8302c3fc20bf4a4264798e08fcaee0a00220e8fb
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2026-05-22 19:02:02 +0530

    env added for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: { "@": path.resolve(__dirname, "./src") },
-  // },
   server: {
     proxy: {
-      // This tells Vite to intercept any request starting with /api
       '/api': {
-        target: 'http://localhost:5000',
+        target: import.meta.env.VITE_API_URL, // use the env variable here
         changeOrigin: true,
-        secure: false,
+        secure: true, // false only if dev has self-signed SSL
       },
     },
   },
